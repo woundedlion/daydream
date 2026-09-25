f83332528408fbac8e1e1d746d64af624f0a9223
Gabriel Levy <gabelevy@gmail.com>
2026-09-24 17:54:06 -0700

    tests: remove UTF-8 byte order marks (finding 296)

--- tests/chain_presentation.test.js
+++ tests/chain_presentation.test.js
@@ -1,4 +1,4 @@
-﻿import { test } from 'node:test';
+import { test } from 'node:test';
 import assert from 'node:assert/strict';
 import { createChainPresentation } from '../tools/chain_presentation.js';
 import { titleCase } from '../tools/labels.js';
